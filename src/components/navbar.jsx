8c8b98e377c85f1726320578e9b96a4418105026
ZachTheMighty <promoted66@gmail.com>
2026-09-18 15:40:08 +0300

    remove: create button in navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,5 +1,4 @@
 import { UserRound } from "lucide-react";
-import { Plus } from "lucide-react";
 import { Link } from "react-router";
 
 export default function Navbar({ userName }) {
@@ -11,17 +10,8 @@ export default function Navbar({ userName }) {
       >
         Welcome back, {userName}
       </Link>
-      <div className="flex justify-center items-center mt-8 gap-8 sm:m-0">
-        <Link
-          to="posts/new"
-          className="flex gap-2 hover:bg-gray-200 px-4 py-2 rounded-full active:bg-gray-300"
-        >
-          <Plus />
-          <div>Create</div>
-        </Link>
-        <div className="p-2 hover:bg-gray-200 active:bg-gray-300 rounded-full">
-          <UserRound />
-        </div>
+      <div className="p-2 hover:bg-gray-200 active:bg-gray-300 rounded-full">
+        <UserRound />
       </div>
     </div>
   );
